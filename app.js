8532793518cbb312f96503b13f21dc230ca4a6b7
Ernesto Martinez Escobar <gagamart@Gabriels-MacBook-Pro.local>
2021-10-22 20:19:50 -0500

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 3005;
 const session = require('express-session');
 
 app.set('view engine', 'pug');
